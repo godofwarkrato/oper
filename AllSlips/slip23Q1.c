f3483263037f2d2a86df1c8e66660640f3d7473f
godofwarkrato <133224959+godofwarkrato@users.noreply.github.com>
2023-05-11 23:13:11 +0530

    Add files via upload

---
+++ AllSlips/slip23Q1.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+int A[10][10], M[10][10], N[10][10];
+int AV[10], Safe[10], Finish[10], R[10][10], nor, nop;
+void AcceptData(int X[10][10])
+{
+    int i, j;
+    for (i = 0; i < nop; i++)
+    {
+        printf("P%d\n", i);
+        for (j = 0; j < nor; j++)
+        {
+            printf("%c:", 65 + j);
+            scanf("%d",&X[i][j]);
+        }
+    }
+}
+void AcceptAvailable()
+{
+    int i;
+    for (i = 0; i < nor; i++)
+    {
+        printf("%c:", 65 + i);
+        scanf("%d", &AV[i]);
+    }
+}
+void AcceptRequest(int R[10][10])
+{
+    int i;
+    for (i = 0; i < nor; i++)
+    {
+        printf("%c:", 65 + i);
+        scanf("%d", &R[i]);
+    }
+}
+void DisplayData()
+{
+    int i, j;
+    printf("\n\tAllocation\t\tMax\t\tNeed\n");
+    printf("\t");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < nor; j++)
+        {
+            printf("%4c", 65 + j);
+        }
+        printf("\t");
+    }
+    for (i = 0; i < nop; i++)
+
+    {
+        printf("\nP%d\t", i);
+        for (j = 0; j < nor; j++)
+        {
+            printf("%4d", A[i][j]);
+        }
+        printf("\t");
+
+        for (j = 0; j < nor; j++)
+        {
+            printf("%4d", M[i][j]);
+        }
+        printf("\t");
+
+        for (j = 0; j < nor; j++)
+            printf("%4d", N[i][j]);
+    }
+    printf("\nAvailable\n");
+    for (j = 0; j < nor; j++)
+    {
+        printf("%4d", AV[j]);
+    }
+}
+
+void CalcNeed()
+{
+    int i, j;
+    for (i = 0; i < nop; i++)
+    {
+        for (j = 0; j < nor; j++)
+        {
+            N[i][j] = M[i][j] - A[i][j];
+        }
+    }
+}
+int CheckNeed(int nop)
+{
+    int i;
+    for (i = 0; i < nor; i++)
+    {
+        if (N[nop][i] > AV[i])
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    printf("\nEnter Number of Process :");
+    scanf("%d", &nop);
+    printf("\nEnter Number of Resources :");
+    scanf("%d", &nor);
+    printf("Enter Allocation\n");
+    AcceptData(A);
+    printf("Enter MAX Requirement\n");
+    AcceptData(M);
+    printf("Enter Availability\n");
+    AcceptAvailable();
+    CalcNeed();
+    DisplayData();
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ AllSlips/slip23Q2.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+int main()
+{
+    int RQ[100], i, n, TotalHeadMovement = 0, initial, count = 0;
+    printf("Enter the number of Requests\n");
+    scanf("%d", &n);
+    printf("Enter the Requests sequence\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &RQ[i]);
+    printf("Enter initial head position\n");
+    scanf("%d", &initial);
+    // sstf disk scheduling
+
+    while (count != n)
+    {
+        int min = 1000, d, index;
+        for (i = 0; i < n; i++)
+        {
+            d = abs(RQ[i] - initial);
+            if (min > d)
+            {
+                min = d;
+                index = i;
+            }
+        }
+        TotalHeadMovement = TotalHeadMovement + min;
+        initial = RQ[index];
+        // 1000 is for max
+        // you can use any number
+        RQ[index] = 1000;
+        count++;
+    }
+    printf("Total head movement is %d", TotalHeadMovement);
+    return 0;
+}
